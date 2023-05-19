4a885da2bd67f649c90794a7ad0141c2f8eede6b
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2023-05-19 22:14:50 +0200

    fix ui

--- modules/cal/client/src/main/scala/forms/Forms.scala
+++ modules/cal/client/src/main/scala/forms/Forms.scala
@@ -14,7 +14,7 @@ import scala.util.Failure
 import scala.util.Success
 
 object Forms {
-  inline given Form[CurrencyCode] with
+  given Form[CurrencyCode] with
     override def render(
         variable: Var[CurrencyCode],
         syncParent: () => Unit,
@@ -28,43 +28,42 @@ object Forms {
           syncParent()
         }
       )
-
-  // inline given Form[IronType[Double, Positive]] with
-  //   def render(
-  //       variable: Var[IronType[Double, Positive]]
-  //   ): HtmlElement =
-  //     val errorVar = Var("")
-  //     div(
-  //       div(child <-- errorVar.signal.map { item =>
-  //         div(
-  //           s"$item"
-  //         )
-  //       }),
-  //       Input(
-  //         _.showClearIcon := true,
-  //         backgroundColor <-- errorVar.signal.map {
-  //           case "" => "white"
-  //           case _  => "red"
-  //         },
-  //         value <-- variable.signal.map(_.toString()),
-  //         onInput.mapToValue
-  //           .filter { str =>
-  //             str.toDoubleOption match
-  //               case None =>
-  //                 errorVar.set("Not a number")
-  //                 false
-  //               case Some(double) =>
-  //                 double.refineEither[Positive] match
-  //                   case Left(error) =>
-  //                     errorVar.set(error)
-  //                     false
-  //                   case Right(value) =>
-  //                     errorVar.set("")
-  //                     false
-  //           }
-  //           .map { case str =>
-  //             str.toDouble.refine[Positive]
-  //           } --> variable.writer
-  //       )
-  //     )
+    // inline given Form[IronType[Double, Positive]] with
+    //   def render(
+    //       variable: Var[IronType[Double, Positive]]
+    //   ): HtmlElement =
+    //     val errorVar = Var("")
+    //     div(
+    //       div(child <-- errorVar.signal.map { item =>
+    //         div(
+    //           s"$item"
+    //         )
+    //       }),
+    //       Input(
+    //         _.showClearIcon := true,
+    //         backgroundColor <-- errorVar.signal.map {
+    //           case "" => "white"
+    //           case _  => "red"
+    //         },
+    //         value <-- variable.signal.map(_.toString()),
+    //         onInput.mapToValue
+    //           .filter { str =>
+    //             str.toDoubleOption match
+    //               case None =>
+    //                 errorVar.set("Not a number")
+    //                 false
+    //               case Some(double) =>
+    //                 double.refineEither[Positive] match
+    //                   case Left(error) =>
+    //                     errorVar.set(error)
+    //                     false
+    //                   case Right(value) =>
+    //                     errorVar.set("")
+    //                     false
+    //           }
+    //           .map { case str =>
+    //             str.toDouble.refine[Positive]
+    //           } --> variable.writer
+    //       )
+    //     )
 }
